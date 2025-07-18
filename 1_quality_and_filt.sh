53ca2782b91496abeee6f7d236966ffbd7501f8c
Gabrieli Bovi dos Santos <68918695+GabrieliBovi@users.noreply.github.com>
2025-07-18 15:47:22 -0300

    Update 1_quality_and_filt.sh

--- 1_quality_and_filt.sh
+++ 1_quality_and_filt.sh
@@ -55,18 +55,21 @@ conda activate nanotrim
 echo "Creating output directories for filtered reads..."
 mkdir -p filt_reads/porechop
 mkdir -p filt_reads/nanofilt
+mkdir -p filt_reads/filtlong
 
-# Run Porechop and NanoFilt on each sample
+# Run Porechop, NanoFilt, and Filtlong on each sample
 for sample in "$INPUT_DIR"/*.fastq.gz; do
     name=$(basename "$sample" .fastq.gz)
 
     echo "Trimming adapters with Porechop: $name"
-   # porechop -i "$sample" --threads 7 > "filt_reads/porechop/${name}_trimmed.fastq"
+   # porechop -i "$sample" --threads 7 > "filt_reads/porechop/${name}_trim.fastq"
 
     echo "Filtering reads with NanoFilt: $name"
-    NanoFilt --quality 9 --length 1200 --maxlength 1700 \
-        < "filt_reads/porechop/${name}_trimmed.fastq" \
-        > "filt_reads/nanofilt/${name}_filt.fastq"
+   # NanoFilt --quality 8 --length 1200 --maxlength 1700 < "filt_reads/porechop/${name}_trim.fastq" | gzip > "filt_reads/nanofilt/${name}_f.fastq.gz"
+
+    echo "Filtering reads with NanoFilt: $name"
+    filtlong --min_mean_q 8 --keep_percent 80 --min_length 1200 --max_length 1700 "filt_reads/nanofilt/${name}_f.fastq.gz" | gzip > "filt_reads/filtlong/${name}_fil.fastq.gz"
+
 done
 
 conda deactivate
@@ -78,6 +81,7 @@ echo "STEP 3: Post-trimming Quality Check"
 
 # Activate environment for quality tools
 echo "Activating Conda environment: nanoquali"
+source ~/anaconda3/etc/profile.d/conda.sh
 conda activate nanoquali
 
 # Create output directories
@@ -86,16 +90,14 @@ mkdir -p final_quality/nanocomp
 mkdir -p final_quality/nanoplot
 
 # Run NanoPlot on each filtered sample
-for sample in filt_reads/nanofilt/*_filt.fastq; do
-    name=$(basename "$sample" _filt.fastq)
+for sample in filt_reads/filtlong/*_fil.fastq.gz; do
+    name=$(basename "$sample" _fil.fastq.gz)
     echo "Running NanoPlot for: $name"
     NanoPlot --fastq "$sample" -o final_quality/nanoplot/"$name" -t 7
 done
 
 # Run NanoComp on all filtered reads
 echo "Running NanoComp on filtered reads..."
-NanoComp --fastq filt_reads/nanofilt/*_filt.fastq --outdir final_quality/nanocomp --plot violin -t 7
-
-conda deactivate
+NanoComp --fastq filt_reads/filtlong/*_fil.fastq.gz --outdir final_quality/nanocomp --plot violin -t 7
 
 echo "Pipeline completed."
